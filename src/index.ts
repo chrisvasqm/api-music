8512b5c4a49acc47eb27fa60aadeef7e0c6fe876
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-20 23:23:07 -0400

    Reformat code

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
+
 app.use('/', (_: Request, response: Response) => {
     return response.send('Welcome to the Music API')
 })
