5a8f86d6dee878270b46bbbf141149345687689c
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-21 14:12:36 -0400

    Config dotenv

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 
+dotenv.config();
+
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
 });
 
 const connect = async () => await client.connect();
