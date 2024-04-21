30427a9bb581fd9743c5b010db0aef000df8ba1b
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-20 23:19:44 -0400

    Add base route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world');
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use('/', (_: Request, response: Response) => {
+    return response.send('Welcome to the Music API')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Music API started on port ${PORT}`)
+})
