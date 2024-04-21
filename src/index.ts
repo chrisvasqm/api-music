f2db121610e9ca3004ca7ded59f03dd0ad3952df
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-20 23:22:48 -0400

    Add connectivity and security middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
 app.use('/', (_: Request, response: Response) => {
     return response.send('Welcome to the Music API')
 })
